25721bf3e72af9c053ec29a09972c6a7defe1dc7
wodanesdag <wodanesdag@proton.me>
2026-04-12 19:15:12 +0700

    fixup! f2fs: fix out-of-bounds access in sysfs attribute read/write

--- fs/f2fs/sysfs.c
+++ fs/f2fs/sysfs.c
@@ -846,7 +846,8 @@ static struct f2fs_attr f2fs_attr_##_name = {			\
 #define F2FS_RO_ATTR(struct_type, struct_name, name, elname)	\
 	F2FS_ATTR_OFFSET(struct_type, name, 0444,		\
 		f2fs_sbi_show, NULL,				\
-		offsetof(struct struct_name, elname))
+		offsetof(struct struct_name, elname),		\
+		sizeof_field(struct struct_name, elname))
 
 #define F2FS_RW_ATTR(struct_type, struct_name, name, elname)	\
 	F2FS_ATTR_OFFSET(struct_type, name, 0644,		\
